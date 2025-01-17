4be0f19ff7fa42313b88068bad3a407b9ac6017d
caxrxloxs <arias.carlo5678@gmailcom>
2025-01-16 23:31:59 -0300

    cambios

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-def do_twice(func):
-    def wrapper_do_twice(*args, **kwargs):
-        func(*args, **kwargs)
-        func(*args, **kwargs)
-    return wrapper_do_twice
-
-@do_twice
-def greet(name):
-    print(f"Hello, {name}!")
-
-
-greet("World")
+if True:
+   print ("Answer")
+   print ("True")
+else:
+   print ("Answer")
+   print ("False")
